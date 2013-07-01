9be1f9d241cc8c6f3d005d32ac129f2d267d8e00
Bernardo Tuso <btuso.95@gmail.com>
2013-07-01 17:52:35 -0300

    Upload commit

---
+++ src/ogs/main/Main.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package ogs.main;
+
+/**
+ * @author Bernardo Tuso
+ *
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
